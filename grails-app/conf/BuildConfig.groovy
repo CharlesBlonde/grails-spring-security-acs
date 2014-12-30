623aaf1f45f1e28f3457cea3debec6bb4271f139
Charles Blonde <charles.blonde@gmail.com>
2014-12-30 15:06:37 +0100

    Remove ehcache dependency

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -43,7 +43,9 @@ grails.project.dependency.resolution = {
         compile 'org.springframework.security:spring-security-jwt:1.0.1.RELEASE'
         compile 'com.google.guava:guava:17.0'
         compile 'commons-codec:commons-codec:1.9'
-        compile 'net.sf.ehcache:ehcache:2.9.0'
+        build('net.sf.ehcache:ehcache:2.9.0'){
+            export = false
+        }
         build ('org.kuali.maven.wagons:maven-s3-wagon:1.2.1') {
             export = false
             exclude 'slf4j-log4j12'
